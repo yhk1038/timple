cd3851e096bbd194d47eac01ac23cb430f3b2535
fred d kim <yhk1038@hanmail.com>
2017-05-19 14:42:45 +0900

    landing second

--- app/controllers/util_controller.rb
+++ app/controllers/util_controller.rb
@@ -1,4 +1,13 @@
 class UtilController < ApplicationController
+    layout '/util/layouts'
+    
     def index
+        redirect_to table_path if user_signed_in?
+    end
+    
+    def landing
+        redirect_to table_path if user_signed_in?
+        
+        render layout: false
     end
 end
