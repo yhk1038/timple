648d02e02684f22a2051b4dfe9c9da8930342a8e
fred d kim <yhk1038@hanmail.com>
2017-05-19 14:44:30 +0900

    landing second

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+    
     devise_for :users, controllers: {
             sessions: 'users/sessions'
     }
     
-    root 'util#index'
+    root 'util#landing'
     get 'util/index'
+    get 'timetable/intro', to: 'timetables#intro', as: 'table'
+
+    resources :groups do
+        resources :timetables
+        resources :marks
+    end
 end
